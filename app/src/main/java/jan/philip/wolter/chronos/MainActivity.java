f5e72f71cbc5cdd96422bcf3cc94b4e9ae47949e
Haifrit <janphilipwolter@gmail.com>
2016-06-09 17:40:44 +0200

    Recycler View is now using staggeredGridLayoutManager , Tiles now wrap content

--- app/src/main/java/jan/philip/wolter/chronos/MainActivity.java
+++ app/src/main/java/jan/philip/wolter/chronos/MainActivity.java
@@ -1,9 +1,11 @@
 package jan.philip.wolter.chronos;
 
+import android.graphics.drawable.GradientDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,10 +15,15 @@ public class MainActivity extends AppCompatActivity {
   RecyclerView myRecyclerView;
   MyRecyclerViewAdapter myRecyclerViewAdapter;
   LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+  StaggeredGridLayoutManager staggeredGridLayoutManager = new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL);
   List<MyDate> listOfMyDates = new ArrayList<>();
   MyDate dateOne = new MyDate(2000,1,1);
   MyDate dateTwo = new MyDate(3000,2,2);
   MyDate dateThree = new MyDate(1999,2,2);
+  MyDate dateFour = new MyDate(2001,1,1);
+  MyDate dateFive = new MyDate(3001,2,2);
+  MyDate dateSix = new MyDate(1998,2,2);
+
 
 
   @Override
@@ -24,9 +31,12 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     myRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
-    myRecyclerView.setLayoutManager(linearLayoutManager);
+    myRecyclerView.setLayoutManager(staggeredGridLayoutManager);
     listOfMyDates.add(dateOne);
     listOfMyDates.add(dateTwo);
+    listOfMyDates.add(dateFour);
+    listOfMyDates.add(dateFive);
+    listOfMyDates.add(dateSix);
     listOfMyDates.add(dateThree);
     myRecyclerViewAdapter = new MyRecyclerViewAdapter(listOfMyDates);
     myRecyclerView.setAdapter(myRecyclerViewAdapter);
